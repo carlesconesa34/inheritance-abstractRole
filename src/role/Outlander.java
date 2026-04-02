0db9dcaaff8eecd24b4a4e37fde491fa92f7e7c7
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-04-02 19:06:48 +0200

    Implement Outlander public class

--- src/role/Outlander.java
+++ src/role/Outlander.java
@@ -7,20 +7,34 @@
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
  */
- 
-/**
- * 
- */
 
-package role; //Package declaration.
+package role;
 
-/**
- * 
- */
+public class Outlander extends Role {
 
-public class Outlander {
-	public static void main(String args[]) {
+    public Outlander(float posX, float posY) {
+        super(posX, posY);
+    }
 
-	}
+    @Override
+    public void move() {
+        if (isAvailable() && getEnergy() >= 0.5f) {
+            setPosX(getPosX() + 8.5f);
+            setEnergy(getEnergy() - 0.5f);
+        }
+    }
+
+    @Override
+    public void jump() {
+        if (isAvailable() && getEnergy() >= 1.0f) {
+            setPosY(getPosY() + 6.2f);
+            setEnergy(getEnergy() - 1.0f);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Outlander -> " + super.toString();
+    }
 }
 

9c0573145a3f3b62f25cb0cb8d11e4817c57d9f7
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-04-06 15:30:09 +0200

    Add Javadoc comments

--- src/role/Outlander.java
+++ src/role/Outlander.java
@@ -10,12 +10,35 @@
 
 package role;
 
+/**
+ * Concrete implementation of Role representing an Outlander character.
+ *
+ * Each action consumes energy defined in Utils. The action is only performed
+ * when the role is available and has sufficient energy.
+ *
+ * @author Carles Conesa
+ * @version 1.0
+ * @since 2026-04-02
+ */
 public class Outlander extends Role {
 
+    /**
+     * Creates a new Outlander at the specified position on the 2-D plane.
+     *
+     * @param posX initial horizontal position
+     * @param posY initial vertical position
+     */
     public Outlander(float posX, float posY) {
         super(posX, posY);
     }
 
+    /**
+     * Moves the Outlander a fixed number of units along the X axis and
+     * consumes the corresponding energy points.
+     *
+     * The action is only performed if the role is available and has sufficient
+     * energy remaining.
+     */
     @Override
     public void move() {
         if (isAvailable() && getEnergy() >= Utils.ENERGY_MOVE) {
@@ -24,6 +47,13 @@ public class Outlander extends Role {
         }
     }
 
+    /**
+     * Makes the Outlander jump a fixed number of units along the Y axis and
+     * consumes the corresponding energy points.
+     *
+     * The action is only performed if the role is available and has sufficient
+     * energy remaining.
+     */
     @Override
     public void jump() {
         if (isAvailable() && getEnergy() >= Utils.ENERGY_JUMP) {
@@ -32,6 +62,12 @@ public class Outlander extends Role {
         }
     }
 
+    /**
+     * Returns a string representation of this Outlander, prefixed with
+     * the character type and followed by the common Role state.
+     *
+     * @return a string representation of the object
+     */
     @Override
     public String toString() {
         return "Outlander -> " + super.toString();
